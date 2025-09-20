b779934930627bc3aed8cb286ad279b75d948cd1
Karen <Karenheister@gmail.com>
2025-09-20 12:46:14 +0300

    вынес всё в отдельные виджеты

--- lib/widgets/main_screen/main_screen_widget.dart
+++ lib/widgets/main_screen/main_screen_widget.dart
@@ -2,6 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_kinopoisk_clone/Theme/app_colors.dart';
 import 'package:flutter_kinopoisk_clone/Theme/app_text_style.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Kinopoisk Clone',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.mainColorBlack,
+        ),
+        scaffoldBackgroundColor: AppColors.mainColorBlack,
+      ),
+      home: const MainScreenWidget(),
+    );
+  }
+}
+
+// ------------------------- MAIN SCREEN -------------------------
 class MainScreenWidget extends StatefulWidget {
   const MainScreenWidget({super.key});
 
@@ -11,40 +35,7 @@ class MainScreenWidget extends StatefulWidget {
 
 class _MainScreenWidgetState extends State<MainScreenWidget> {
   int currentPageIndex = 0;
-  int currentSabTabIndex = 0;
-
-  final List<NavigationDestinatioinData> navData = [
-    NavigationDestinatioinData(
-      icon: Icons.home_filled,
-      labelText: 'Главное',
-      selectedIcon: Icons.home_filled,
-      color: AppColors.mainColorDeepOrange,
-    ),
-    NavigationDestinatioinData(
-      icon: Icons.add_to_photos,
-      labelText: 'Билеты',
-      selectedIcon: Icons.add_to_photos,
-      color: AppColors.mainColorDeepOrange,
-    ),
-    NavigationDestinatioinData(
-      icon: Icons.bookmarks_outlined,
-      labelText: 'Моё',
-      selectedIcon: Icons.bookmarks_outlined,
-      color: AppColors.mainColorDeepOrange,
-    ),
-    NavigationDestinatioinData(
-      icon: Icons.search,
-      labelText: 'Поиск',
-      selectedIcon: Icons.search,
-      color: AppColors.mainColorDeepOrange,
-    ),
-    NavigationDestinatioinData(
-      icon: Icons.person_add_alt_rounded,
-      labelText: 'Профиль',
-      selectedIcon: Icons.person_add_alt_rounded,
-      color: AppColors.mainColorDeepOrange,
-    ),
-  ];
+  int currentSubTabIndex = 0;
 
   static const List<String> _subTabs = [
     'Моё кино',
@@ -55,7 +46,7 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
     'Загрузить',
   ];
 
-  static const List<Widget> _mainSubTabWidgets = <Widget>[
+  static const List<Widget> _subTabWidgets = [
     Center(child: Text('Моё кино', style: AppTextStyle.textStyleBlack)),
     Center(child: Text('Детям', style: AppTextStyle.textStyleBlack)),
     Center(child: Text('Спорт', style: AppTextStyle.textStyleBlack)),
@@ -64,102 +55,105 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
     Center(child: Text('Загрузить', style: AppTextStyle.textStyleBlack)),
   ];
 
-  Widget? _buildAppBar() {
-    switch (currentPageIndex) {
-      case 0: // Главное
-        return AppBar(
-          titleSpacing: 0,
-          title: Row(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(right: 16),
-                child: Icon(
-                  Icons.movie,
-                  color: AppColors.mainColorDeepOrange,
-                ), // лого
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: List.generate(_subTabs.length, (i) {
-                      final isSelected = currentSabTabIndex == i;
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8),
-                        child: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              currentSabTabIndex = i;
-                            });
-                          },
-                          child: Text(
-                            _subTabs[i],
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: isSelected
-                                  ? AppColors.mainColorDeepOrange
-                                  : Colors.white,
-                            ),
+  final List<NavigationDestinatioinData> navData = [
+    NavigationDestinatioinData(
+      icon: Icons.home_filled,
+      selectedIcon: Icons.home_filled,
+      labelText: 'Главное',
+      color: AppColors.mainColorDeepOrange,
+    ),
+    NavigationDestinatioinData(
+      icon: Icons.add_to_photos,
+      selectedIcon: Icons.add_to_photos,
+      labelText: 'Билеты',
+      color: AppColors.mainColorDeepOrange,
+    ),
+    NavigationDestinatioinData(
+      icon: Icons.bookmarks_outlined,
+      selectedIcon: Icons.bookmarks_outlined,
+      labelText: 'Моё',
+      color: AppColors.mainColorDeepOrange,
+    ),
+    NavigationDestinatioinData(
+      icon: Icons.search,
+      selectedIcon: Icons.search,
+      labelText: 'Поиск',
+      color: AppColors.mainColorDeepOrange,
+    ),
+    NavigationDestinatioinData(
+      icon: Icons.person_add_alt_rounded,
+      selectedIcon: Icons.person_add_alt_rounded,
+      labelText: 'Профиль',
+      color: AppColors.mainColorDeepOrange,
+    ),
+  ];
+
+  PreferredSizeWidget _buildAppBar() {
+    if (currentPageIndex == 0) {
+      // Главная вкладка с подтабами
+      return AppBar(
+        titleSpacing: 0,
+        title: Row(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(right: 16),
+              child: Icon(Icons.movie, color: AppColors.mainColorDeepOrange),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(_subTabs.length, (i) {
+                    final isSelected = currentSubTabIndex == i;
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            currentSubTabIndex = i;
+                          });
+                        },
+                        child: Text(
+                          _subTabs[i],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: isSelected
+                                ? AppColors.mainColorDeepOrange
+                                : Colors.white,
                           ),
                         ),
-                      );
-                    }),
-                  ),
+                      ),
+                    );
+                  }),
                 ),
               ),
-            ],
-          ),
-        );
-      case 1: // Билеты
-        return AppBar(
-          title: const Text(
-            'Билеты',
-            style: TextStyle(color: AppColors.mainColorWhite),
-          ),
-        );
-      case 2: // Моё
-        return AppBar(
-          title: const Text(
-            'Моё',
-            style: TextStyle(color: AppColors.mainColorWhite),
-          ),
-        );
-      case 3: // Поиск
-        return AppBar(
-          title: const Text(
-            'Поиск',
-            style: TextStyle(color: AppColors.mainColorWhite),
-          ),
-        );
-      case 4: // Профиль
-        return AppBar(
-          title: const Text(
-            'Профиль',
-            style: TextStyle(color: AppColors.mainColorWhite),
-          ),
-        );
-      default:
-        return AppBar(
-          title: const Text(
-            'Kinopoisk Clone',
-            style: AppTextStyle.textStyleWhite,
-          ),
-        );
+            ),
+          ],
+        ),
+      );
+    } else {
+      // Остальные вкладки
+      String title = navData[currentPageIndex].labelText;
+      return AppBar(
+        title: Text(
+          title,
+          style: const TextStyle(color: AppColors.mainColorWhite),
+        ),
+      );
     }
   }
 
   Widget _buildBody() {
     if (currentPageIndex == 0) {
-      return _mainSubTabWidgets[currentSabTabIndex];
+      return _subTabWidgets[currentSubTabIndex];
     }
+
+    // Остальные вкладки
     switch (currentPageIndex) {
       case 1:
         return const Center(
-          child: Text(
-            'Билеты',
-            style: TextStyle(color: AppColors.mainColorBlack),
-          ),
+          child: Text('Билеты', style: TextStyle(color: AppColors.mainColorBlack)),
         );
       case 2:
         return const Center(
@@ -167,73 +161,259 @@ class _MainScreenWidgetState extends State<MainScreenWidget> {
         );
       case 3:
         return const Center(
-          child: Text(
-            'Поиск',
-            style: TextStyle(color: AppColors.mainColorBlack),
-          ),
+          child: Text('Поиск', style: TextStyle(color: AppColors.mainColorBlack)),
         );
       case 4:
         return const Center(
-          child: Text(
-            'Профиль',
-            style: TextStyle(color: AppColors.mainColorBlack),
-          ),
+          child: Text('Профиль', style: TextStyle(color: AppColors.mainColorBlack)),
         );
       default:
         return const Center(
-          child: Text(
-            'Главное',
-            style: TextStyle(color: AppColors.mainColorBlack),
-          ),
+          child: Text('Главное', style: TextStyle(color: AppColors.mainColorBlack)),
         );
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 6,
-      child: Scaffold(
-        backgroundColor: AppColors.mainColorWhite,
-        appBar: _buildAppBar() as PreferredSizeWidget,
-        bottomNavigationBar: NavigationBar(
-          onDestinationSelected: (int index) {
-            if (currentPageIndex == index) return;
-            setState(() {
-              currentPageIndex = index;
-            });
-          },
-          indicatorColor: AppColors.mainColorBlack,
-          selectedIndex: currentPageIndex,
-          destinations: navData
-              .map(
-                (data) => NavigationDestination(
-                  icon: Icon(data._icon),
-                  label: data._labelText,
-                  selectedIcon: Icon(data._selectedIcon, color: data._color),
+    return Scaffold(
+      backgroundColor: AppColors.mainColorWhite,
+      appBar: _buildAppBar(),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          if (currentPageIndex == index) return;
+          setState(() {
+            currentPageIndex = index;
+            if (currentPageIndex != 0) {
+              currentSubTabIndex = 0; // сброс субвкладки при смене вкладки
+            }
+          });
+        },
+        indicatorColor: AppColors.mainColorBlack,
+        selectedIndex: currentPageIndex,
+        destinations: navData
+            .map(
+              (data) => NavigationDestination(
+                icon: Icon(data.icon),
+                selectedIcon: Icon(data.selectedIcon, color: data.color),
+                label: data.labelText,
+              ),
+            )
+            .toList(),
+      ),
+      body: _buildBody(),
+    );
+  }
+}
+// ------------------------- DATA CLASS -------------------------
+class NavigationDestinatioinData {
+  final String labelText;
+  final IconData selectedIcon;
+  final IconData icon;
+  final Color color;
+
+  const NavigationDestinatioinData({
+    required this.labelText,
+    required this.selectedIcon,
+    required this.icon,
+    required this.color,
+  });
+}
+
+class MainTabScreen extends StatefulWidget {
+  const MainTabScreen({super.key});
+
+  @override
+  State<MainTabScreen> createState() => _MainTabScreenState();
+}
+
+class _MainTabScreenState extends State<MainTabScreen> {
+  int currentSubTabIndex = 0;
+
+  static const List<String> subTabs = [
+    'Моё кино',
+    'Детям',
+    'Спорт',
+    'Каналы',
+    'Подписки',
+    'Загрузить',
+  ];
+
+  static const List<Widget> subTabWidgets = [
+    Center(child: Text('Моё кино', style: AppTextStyle.textStyleWhite)),
+    Center(child: Text('Детям', style: AppTextStyle.textStyleWhite)),
+    Center(child: Text('Спорт', style: AppTextStyle.textStyleWhite)),
+    Center(child: Text('Каналы', style: AppTextStyle.textStyleWhite)),
+    Center(child: Text('Подписки', style: AppTextStyle.textStyleWhite)),
+    Center(child: Text('Загрузить', style: AppTextStyle.textStyleWhite)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 0,
+        title: Row(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(right: 16),
+              child: Icon(Icons.movie, color: AppColors.mainColorDeepOrange),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(subTabs.length, (i) {
+                    final isSelected = currentSubTabIndex == i;
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            currentSubTabIndex = i; // <-- обновляем индекс
+                          });
+                        },
+                        child: Text(
+                          subTabs[i],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: isSelected
+                                ? AppColors.mainColorDeepOrange
+                                : Colors.white,
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
                 ),
-              )
-              .toList(),
+              ),
+            ),
+          ],
         ),
-        body: _buildBody(),
+      ),
+      body: subTabWidgets[currentSubTabIndex], // <-- тело меняется
+    );
+  }
+}
+
+// ------------------------- MAIN TAB APPBAR -------------------------
+class MainTabAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const MainTabAppBar({super.key});
+
+  @override
+  State<MainTabAppBar> createState() => _MainTabAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _MainTabAppBarState extends State<MainTabAppBar> {
+  int currentSubTabIndex = 0;
+
+  static const List<String> subTabs = [
+    'Моё кино',
+    'Детям',
+    'Спорт',
+    'Каналы',
+    'Подписки',
+    'Загрузить',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      titleSpacing: 0,
+      title: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(Icons.movie, color: AppColors.mainColorDeepOrange),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(subTabs.length, (i) {
+                  final isSelected = currentSubTabIndex == i;
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          currentSubTabIndex = i;
+                        });
+
+                        // Находим MainTabScreen и обновляем его состояние
+                        final mainTabState = context
+                            .findAncestorStateOfType<_MainTabScreenState>();
+                        mainTabState?.setState(() {
+                          mainTabState.currentSubTabIndex = i;
+                        });
+                      },
+                      child: Text(
+                        subTabs[i],
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: isSelected
+                              ? AppColors.mainColorDeepOrange
+                              : Colors.white,
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-class NavigationDestinatioinData {
-  final String _labelText;
-  final IconData _selectedIcon;
-  final IconData _icon;
-  final Color _color;
+// ------------------------- OTHER SCREENS -------------------------
+class TicketsScreen extends StatelessWidget {
+  const TicketsScreen({super.key});
 
-  NavigationDestinatioinData({
-    required String labelText,
-    required IconData selectedIcon,
-    required IconData icon,
-    required Color color,
-  }) : _labelText = labelText,
-       _selectedIcon = selectedIcon,
-       _icon = icon,
-       _color = color;
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Билеты', style: TextStyle(color: AppColors.mainColorBlack)),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Моё', style: TextStyle(color: AppColors.mainColorBlack)),
+    );
+  }
+}
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Поиск', style: TextStyle(color: AppColors.mainColorBlack)),
+    );
+  }
+}
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Профиль', style: TextStyle(color: AppColors.mainColorBlack)),
+    );
+  }
 }

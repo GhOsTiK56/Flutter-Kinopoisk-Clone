b8530c73db28bb04f0e5ea78d4638237d3d5d59b
GhOsTiK <karenheister5@gmail.com>
2025-10-05 15:14:08 +0300

    Добавил главный экран и нижний bar с вкладками

--- lib/Theme/app_colors.dart
+++ lib/Theme/app_colors.dart
@@ -4,4 +4,5 @@ abstract class AppColors {
   static const mainColorBlack = Color.fromRGBO(0, 0, 0, 1);
   static const mainColorWhite = Color.fromRGBO(255, 255, 255, 1);
   static const mainColorDeepOrange = Color.fromRGBO(255, 87, 34, 1);
+  static const mainColorDarkBlue = Color.fromRGBO(3, 37, 65, 1);
 }
--- lib/widgets/main_screen/main_screen_widget.dart
+++ lib/widgets/main_screen/main_screen_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kinopoisk_clone/Theme/app_colors.dart';
 
 class MainScreenWidget extends StatefulWidget {
   const MainScreenWidget({super.key});
@@ -8,17 +9,46 @@ class MainScreenWidget extends StatefulWidget {
 }
 
 class _MainScreenWidgetState extends State<MainScreenWidget> {
+  int _selectedTab = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Новости'),
+    Text('Фильмы'),
+    Text('Сериалы'),
+  ];
+
+  void onSelectedTab(int index) {
+    if (_selectedTab == index) return;
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.mainColorBlack,
       appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: AppColors.mainColorDarkBlue,
         title: Center(
           child: const Text(
             'Kinopoisk Clone',
-            style: TextStyle(color: Colors.white),
+            style: TextStyle(color: AppColors.mainColorWhite),
           ),
         ),
+      ),
+      body: Center(child: _widgetOptions[_selectedTab]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Новости'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie_creation_outlined),
+            label: 'Фильмы',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.tv), label: 'Сериалы'),
+        ],
+        onTap: onSelectedTab,
       ),
     );
   }
